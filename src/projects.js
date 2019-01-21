ce5cfb735750846c9eb99a417e50884a8d3dd200
Marguerite Roth <marguerite.roth@gmail.com>
2019-01-21 09:41:56 -0500

    update albondiga url

--- src/projects.js
+++ src/projects.js
@@ -3,7 +3,7 @@ const projects = {
         title: "Albondiga | App for requesting some dinner 🍝",
         projectType: "web",
         description: "I developed this project as a birthday gift for my partner — a lover of spaghetti and meatballs. The recipient can pick the ingredients making up a spaghetti and meatballs dinner: pasta type, sauce, drinks, etc.",
-        demo: "https://marguerite.io/albondiga/",
+        demo: "https://margueriteroth.github.io/albondiga/",
         github: "https://github.com/margueriteroth/albondiga",
         "tech": [
             "react",
